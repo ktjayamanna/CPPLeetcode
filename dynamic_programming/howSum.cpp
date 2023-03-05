7688d327939c223b585a20b7d7ccbb371a6d15fc
kjayamanna <kaveen.jayamanna@gmail.com>
2023-03-04 19:36:26 -0500

    Fixed the howSum program

--- dynamic_programming/howSum.cpp
+++ dynamic_programming/howSum.cpp
@@ -1,27 +1,31 @@
-#include <cstddef>
+#include<limits>
 #include <iostream>
 #include <string>
 #include <vector>
 
 
 std::vector<int> howSum(int sum,std::vector<int> arr){
-    if(sum < 0){return nullptr;}
+    int negative_infinity = std::numeric_limits<int>::lowest(); 
+    if(sum < 0){return std::vector<int> {negative_infinity};}
     if(sum == 0){return std::vector<int>();}
     else {
         for(int i=0; i<arr.size();i++){
             std::vector<int> result = howSum(sum - arr[i], arr);
-            if(!result.empty()){
+            if((!result.empty() && result[0] != negative_infinity) || result.empty()){
                 result.push_back(arr[i]);
                 return result;
             }
         }
-        return nullptr;
+        return std::vector<int> {negative_infinity};
     }
 }
 
-int main(){
-    std::vector<int> arr = {1, 2, 3};
-    std::vector<int> result  = howSum(4,  arr);
-    for(int idx = 0; idx<result.size(); idx++){std::cout <<(result[idx]);}
-
+int main() {
+    std::vector<int> arr = {2, 3};
+    std::vector<int> result = howSum(7, arr);
+    for(int idx = 0; idx < result.size(); idx++) {
+        std::cout << result[idx] << " ";  // add space after each number
+    }
+    std::cout << std::endl;  // add newline at the end
+    return 0;
 }
